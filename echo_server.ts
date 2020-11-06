0df33c6fcb01dd5369ab19480fd2741cf078c6e4
Joshua Cline <wwjdtd@gmail.com>
2020-11-06 00:59:48 -0800

    Trying to use example code and failing.

--- echo_server.ts
+++ echo_server.ts
@@ -1,4 +1,4 @@
-import { WebSocket, isWebSocketCloseEvent } from 'https://deno.land/std@0.76.0/ws/mod.ts'
+import { WebSocket, isWebSocketCloseEvent, isWebSocketPingEvent } from 'https://deno.land/std@0.76.0/ws/mod.ts'
 import { v4 } from 'https://deno.land/std@0.76.0/uuid/mod.ts'
 
 export class Echo_Server {
@@ -12,19 +12,39 @@ export class Echo_Server {
         this.broadcast(`${userId} is connected`);
 
         // Wait for new messages
-        for await (const event of ws) {
-            console.log(event + "")
-            console.log(JSON.stringify(event));
-            if (isWebSocketCloseEvent(event)) {
-                this.users.delete(userId);
-                this.broadcast(`${userId} is disconnected`);
-                break;
+        try {
+            for await (const ev of ws) {
+                if (typeof ev === "string") {
+                    // text message
+                    console.log("ws:Text", ev);
+                    if (isWebSocketCloseEvent(ev)) {
+                        this.users.delete(userId);
+                        this.broadcast(`${userId} is disconnected`);
+                        break;
+                    }
+
+                    const message = (typeof ev === "string" ? ev : "");
+
+                    this.broadcast(message, userId);
+                } else if (ev instanceof Uint8Array) {
+                    // binary message
+                    console.log("ws:Binary", ev);
+                } else if (isWebSocketPingEvent(ev)) {
+                    const [, body] = ev;
+                    // ping
+                    console.log("ws:Ping", body);
+                } else if (isWebSocketCloseEvent(ev)) {
+                    // close
+                    const { code, reason } = ev;
+                    console.log("ws:Close", code, reason);
+                }
             }
+        } catch (err) {
+            console.error(`failed to receive frame: ${err}`);
 
-            //const message = camelize(typeof event === "string" ? event : "");
-            const message = (typeof event === "string" ? event : "");
-
-            this.broadcast(message, userId);
+            if (!ws.isClosed) {
+                await ws.close(1000).catch(console.error);
+            }
         }
 
     }
@@ -38,6 +58,9 @@ export class Echo_Server {
         if (!message) return;
         console.log({ message, senderId })
         for (const user of this.users.values()) {
+            if(user.isClosed){
+                continue;
+            }
             user.send(senderId ? `[${senderId}]: ${message}` : message);
         }
     }
