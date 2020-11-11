f585091c1a3ebe5b7b3cf05fa2fab9f3190b5a2e
Joshua Cline <wwjdtd@gmail.com>
2020-11-11 14:51:37 -0800

    Isolated modules fix.

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 import type { Middleware, Context } from "https://deno.land/x/oak@v6.3.1/mod.ts";
-import { WebSocket, acceptable } from 'https://deno.land/std@0.76.0/ws/mod.ts'
-export { WebSocket };
+import { WebSocket, acceptable } from 'https://deno.land/std@0.76.0/ws/mod.ts';
 export type handler = (socket: WebSocket, url: URL, headers: Headers) => Promise<void>;
 export class WebSocketMiddleware {
     public handler: handler;
