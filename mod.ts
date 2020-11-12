2fd83ef1a33f4d5ec9553cbf0e50173d213a5666
Joshua Cline <wwjdtd@gmail.com>
2020-11-11 20:26:34 -0800

    Added actual documentation.

--- mod.ts
+++ mod.ts
@@ -1,8 +1,18 @@
 import type { Middleware } from 'https://deno.land/x/oak@v6.3.2/mod.ts';
 import type { WebSocket } from 'https://deno.land/std@0.77.0/ws/mod.ts';
 import { acceptable } from 'https://deno.land/std@0.77.0/ws/mod.ts';
+
+/**
+ * Handles incoming websocket connections.
+ * URL and headers are for the initial HTTP request that the WebSocket has been upgraded from.
+ */
 export type handler = (socket: WebSocket, url: URL, headers: Headers) => Promise<void>;
 
+/**
+ * "Idiot's guide to oak WebSockets"
+ * Put as the top level middleware to enable WS connections to any server url. Filtering is done within the passed handler. 
+ * @param handle A handler that will process all incoming WebSockets.
+ */
 export function WebSocketMiddleware(handle: handler): Middleware {
     return async function (ctx, next) {
         if (acceptable(ctx.request)) {
